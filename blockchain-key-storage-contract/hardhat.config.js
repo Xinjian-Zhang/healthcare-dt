f4f8f951b4b6672fe591483d906008fdff96577c
Xinjian-Zhang <1xinjian.zhang@gmail.com>
2025-05-02 23:19:26 +0300

    feat(blockchain-key-storage-contract): migrate and integrate complete contract project

---
+++ blockchain-key-storage-contract/hardhat.config.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
+
+module.exports = {
+  solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`], 
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
+};
